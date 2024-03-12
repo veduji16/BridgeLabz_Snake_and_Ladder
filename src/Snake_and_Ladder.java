f408f73b01ef22b651619b0411051fb7ef881c63
veduji16 <jainved01@gmail.com>
2024-03-12 12:19:48 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake.

--- src/Snake_and_Ladder.java
+++ src/Snake_and_Ladder.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class Snake_and_Ladder {
-    private static int randomNum() {
+    public static int randomNum() {
         Random dice = new Random();
         return dice.nextInt(6) + 1;
     }
-    private static int checkOption() {
+    public static int checkOption() {
         Random option = new Random();
         return option.nextInt(3) + 1;
     }
@@ -13,41 +13,28 @@ public class Snake_and_Ladder {
     public static void main(String[] args) {
         int start;
         int position = 0;
-        int goal = 100;
         start = position;
 
         System.out.println("\n...Welcome to the Snake & Ladders Game...\n");
         System.out.println("Your Start Postion: " + start);
 
-        while (position < goal) {
-            int diceValue = randomNum();
+        System.out.println("Dice rolled to:  " + randomNum());
 
-            System.out.println("Dice rolled to: " + diceValue);
+        int option = checkOption();
+        switch (option) {
+            case 1:
+                System.out.println("No Play");
+                break;
 
-            int option = checkOption();
-            switch (option) {
-                case 1:
-                    System.out.println("No Play");
-                    break;
+            case 2:
+                System.out.println("Ladder");
+                position = position + randomNum();
+                break;
 
-                case 2:
-                    System.out.println("Ladder");
-                    position += diceValue;
-                    break;
-
-                case 3:
-                    System.out.println("Snake");
-                    position -= diceValue;
-                    break;
-            }
-
-            if (position < 0) {
-                System.out.println("...Game is restarted...");
-                position = 0;
-            } else if (position > goal) {
-                position -= diceValue;
-            }
+            case 3:
+                System.out.println("Snake");
+                position = position - randomNum();
+                break;
         }
-        System.out.println("Congrats! You Won.");
     }
 }
