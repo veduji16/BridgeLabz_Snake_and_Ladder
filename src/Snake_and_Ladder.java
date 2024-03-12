c39c8566b732f5efd53025c67229e56a965b6291
veduji16 <jainved01@gmail.com>
2024-03-12 11:59:25 +0530

    The player rolls the die to get the number between 1 to 6

--- src/Snake_and_Ladder.java
+++ src/Snake_and_Ladder.java
@@ -1,4 +1,11 @@
+import java.util.*;
+
 public class Snake_and_Ladder {
+    public static int randomNum() {
+        Random dice = new Random();
+        return dice.nextInt(6) + 1;
+    }
+
     public static void main(String[] args) {
         int start;
         int position = 0;
@@ -6,5 +13,6 @@ public class Snake_and_Ladder {
 
         System.out.println("\n...Welcome to the Snake & Ladders Game...\n");
         System.out.println("Your Start Postion: " + start);
+        System.out.println("Dice rolled to:  " + randomNum());
     }
 }
