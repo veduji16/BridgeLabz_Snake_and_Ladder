f24572274cfe223e154c7d247a2a83dc542b96b1
veduji16 <jainved01@gmail.com>
2024-03-12 12:08:56 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake.

--- src/Snake_and_Ladder.java
+++ src/Snake_and_Ladder.java
@@ -6,6 +6,11 @@ public class Snake_and_Ladder {
         return dice.nextInt(6) + 1;
     }
 
+    public static int checkOption() {
+        Random option = new Random();
+        return option.nextInt(3) + 1;
+    }
+
     public static void main(String[] args) {
         int start;
         int position = 0;
@@ -13,6 +18,24 @@ public class Snake_and_Ladder {
 
         System.out.println("\n...Welcome to the Snake & Ladders Game...\n");
         System.out.println("Your Start Postion: " + start);
+
         System.out.println("Dice rolled to:  " + randomNum());
+
+        int option = checkOption();
+        switch (option) {
+            case 1:
+                System.out.println("No Play");
+                break;
+
+            case 2:
+                System.out.println("Ladder");
+                position = position + randomNum();
+                break;
+
+            case 3:
+                System.out.println("Snake");
+                position = position - randomNum();
+                break;
+        }
     }
 }
